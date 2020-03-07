e6bb3e7c40b9ef4d0d85b77d34538dffcafd42fc
Alexander Walther <github.com@alexplus.de>
2020-03-07 15:46:33 +0100

    Mindestanforderungen hochsetzen
    und config zukünftig durch REDAXO schreiben lassen

--- install.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if(!rex_config::has("yrewrite_url_redirect")) {
-    rex_config::set("yrewrite_url_redirect", "force_trailing_slash", 1);
-    rex_config::set("yrewrite_url_redirect", "force_forward", 1);
-}
